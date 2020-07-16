8f38f42d8c5c3c6b6ea74df19891e847cddd245b
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2020-07-16 21:31:53 +0300

    Transparency exceptions created

---
+++ src/exceptions.js
@@ -0,0 +1,8 @@
+// Exception list to preserve transparency of the scheme properties.
+const exceptions = [
+  'scrollbarSlider.activeBackground',
+  'scrollbarSlider.background',
+  'scrollbarSlider.hoverBackground',
+];
+
+module.exports = exceptions;
