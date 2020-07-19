5907462575247b53f5c1284bae6801d142c7697b
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2020-07-19 20:54:12 +0300

    Deep object transformation functionality improved

--- src/transform.js
+++ src/transform.js
@@ -2,16 +2,21 @@
  * Deep object transformer.
  *
  * @param {*} o The object that the transformation function will be applied.
- * @param {*} t The transformation function to be applied.
+ * @param {*} t The test function for the property, takes key and value.
+ * @param {*} f The transformation function to be applied.
  * @return {*} Transform applied object.
  */
-const transform = function (o, t) {
+const transform = function (o, t, f) {
   for (var k in o) {
+
     if (o[k] !== null && typeof o[k] === 'object') {
-      o[k] = transform(o[k], t);
+      o[k] = transform(o[k], t, f);
       continue;
     }
-    o[k] = t.apply(this, [k, o[k]]) || o[k];
+
+    if (typeof t === 'function' && !t.call(this, k, o[k])) continue;
+
+    o[k] = f.apply(this, [k, o[k]]) || o[k];
   }
   return o;
 };
